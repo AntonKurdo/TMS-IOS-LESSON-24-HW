94e36903a584f0d77bbcf83cff9c8ec2cff85c69
mac <mac@macs-MacBook-Pro.local>
2024-02-20 12:27:24 +0300

    project init

---
+++ TMS-IOS-LESSON-24-HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMS-IOS-LESSON-24-HW
+//
+//  Created by mac on 20.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
